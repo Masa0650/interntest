f88486926af40dd61d2eb0530c4e03c2422a70f4
Masa0659 <24rd164@ms.dendai.ac.jp>
2025-01-12 16:53:20 +0900

    型チェックするスクリプトを追加

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
++ let age: number = 30; // number型の変数を定義
++ age = "thirty"; // string型をnumberアサインできないためエラーとなる
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up app.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
